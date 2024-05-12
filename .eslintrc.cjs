00923214b5769720456270020947b7e77a0c603f
backas36 <backas36@gmail.com>
2024-05-12 22:12:07 +0800

    chore: first commit, setup prettier, husky, lint-stage, commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ["react-refresh", "prettier"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "prettier/prettier": "error",
+    },
+};
