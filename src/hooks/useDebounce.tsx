746b9e79d3f0826cd11969924ae7d3705059a7c1
backas36 <backas36@gmail.com>
2024-05-18 15:53:41 +0800

    feat: debounce demo

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useDebounce = <F extends (...args: any[]) => void>(fn: F, delay: number) => {
+    let timeout: number | null = null;
+    return (...args: Parameters<F>) => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+};
+export default useDebounce;
--- src/page/DebounceThrottlePage.tsx
+++ src/page/DebounceThrottlePage.tsx
@@ -1,21 +1,49 @@
-//import { useState } from "react";
+import { useState } from "react";
+
+import useDebounce from "@/hooks/useDebounce";
+
+interface ICountry {
+    name: string;
+}
 
 const DebounceThrottlePage = () => {
-    //const [data, setData] = useState([]);
-    const loadData = (searchInput: string) => {
-        console.log(searchInput);
+    const [data, setData] = useState<ICountry[] | null>(null);
+    const loadData = async (searchInput: string) => {
+        console.count("loadData");
+        if (searchInput === "") {
+            setData(null);
+            return;
+        }
+        const response = await fetch(`https://countriesnow.space/api/v0.1/countries/positions`);
+        const resData = await response.json();
+        const countries = resData.data.filter((c: ICountry) => {
+            if (c.name.toLowerCase().includes(searchInput.toLowerCase())) {
+                return {
+                    name: c.name,
+                };
+            }
+        });
+        setData(countries);
     };
-    const handleLoadDataDebounced = (searchInput: string) => loadData(searchInput);
+    const handleLoadDataDebounced = useDebounce(loadData, 5000);
+    //const handleLoadDataDebounced = loadData;
     return (
         <div>
-            <label className='block text-gray-500 font-bold'>
+            <label className='block font-bold text-gray-500'>
                 Debounce:
                 <input
                     type='text'
                     onChange={(e) => handleLoadDataDebounced(e.target.value)}
-                    className=' appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500 bg-gray-200'
+                    className=' appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none'
                 />
             </label>
+            <div className=''>
+                {data &&
+                    data.length > 0 &&
+                    data.map((country, idx) => {
+                        return <div key={idx}>{country.name}</div>;
+                    })}
+            </div>
         </div>
     );
 };
