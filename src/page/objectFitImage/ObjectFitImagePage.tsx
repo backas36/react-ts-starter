f26b7b918deadd9f6f2fe23d22f8e8266fa03405
backas36 <backas36@gmail.com>
2024-06-13 10:22:03 +0800

    feat: add resposive image

--- src/page/objectFitImage/ObjectFitImagePage.tsx
+++ src/page/objectFitImage/ObjectFitImagePage.tsx
@@ -7,6 +7,11 @@ const ObjectFitImagePage = () => {
                 <h1 className={styles.bannerTitle}>Banner Image Title, Banner Image Title</h1>
                 <img src='https://picsum.photos/id/479/2000/800' alt='banner' className={styles.bannerImg} />
             </div>
+
+            <h2 className='mt-16'>Responsive image</h2>
+            <div className={styles.imgBox}>
+                <img src='https://picsum.photos/id/124/1200/800' alt='' />
+            </div>
         </main>
     );
 };
