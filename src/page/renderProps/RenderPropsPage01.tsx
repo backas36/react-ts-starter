1cb6a13a4932b96a4fc8c3b164a4f71f26364184
backas36 <backas36@gmail.com>
2024-09-05 23:38:56 +0800

    feat: render props to calculate temperature - before

--- src/page/renderProps/RenderPropsPage01.tsx
+++ src/page/renderProps/RenderPropsPage01.tsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+
 const RenderPropsPage01 = () => {
-    return <div>RenderPropsPage01</div>;
+    return (
+        <div>
+            <TemperatureInput />
+            <Kelvin />
+            <Celsius />
+        </div>
+    );
 };
 export default RenderPropsPage01;
+
+const TemperatureInput = () => {
+    const [value, setValue] = useState("");
+    return (
+        <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className='w-1/2 rounded-lg border px-3 py-2 text-gray-700 focus:border-blue-500 focus:outline-none'
+            placeholder='Enter temperature'
+        />
+    );
+};
+
+const Kelvin = ({ value }: { value: number }) => {
+    return (
+        <div className='w-1/2 rounded-md bg-blue-100 p-3 text-lg font-semibold text-blue-600 shadow-sm'>
+            Kelvin: {value}
+        </div>
+    );
+};
+
+const Celsius = ({ value }: { value: number }) => {
+    return (
+        <div className='w-1/2 rounded-md bg-blue-100 p-3 text-lg font-semibold text-blue-600 shadow-sm'>
+            Celsius: {value}
+        </div>
+    );
+};
